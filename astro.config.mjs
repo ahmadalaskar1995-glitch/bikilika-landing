753dd25786e6bd749d5c72c96b09e73908e158ed
Ahmad Al-Askar <ahmadalaskar1995@gmail.com>
2026-04-12 21:12:06 +0300

    تحويل الموقع إلى static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 // @ts-check
-/// <reference types="node" />
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 export default defineConfig({
   site: process.env.URL || 'http://localhost:4321',
-  output: 'server', // يجعل جميع الصفحات SSR (تُبنى عند الطلب)
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'static'
 });
